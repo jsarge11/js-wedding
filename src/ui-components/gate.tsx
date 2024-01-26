e1fe6a1466db2bfcab83fae46eda67c5fc2b0078
Jay Sargent <sargentjay11@gmail.com>
2024-01-26 12:55:59 -0700

    removed some gingham at full width

--- src/ui-components/gate.tsx
+++ src/ui-components/gate.tsx
@@ -8,7 +8,7 @@ interface GateProps {
 export const Gate: FC<GateProps> = ({ setIsComplete }) => {
 	return (
 		<div
-			className="gate bg-cover bg-center min-h-full relative z-50 -mt-10 lg:-mt-72 lg:custom-gate-position"
+			className="gate bg-cover bg-center min-h-full relative z-50 -mt-10 lg:-mt-72 lg:custom-gate-position xl:-mt-96"
 			id="gate">
 			<Form setIsComplete={setIsComplete} />
 		</div>
