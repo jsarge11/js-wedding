12afdd43ea8725029e703ee6b3e3616f145c1a3e
Jay Sargent <sargentjay11@gmail.com>
2024-04-15 19:47:24 -0600

    correct formatting

--- src/ui-components/qr-code.tsx
+++ src/ui-components/qr-code.tsx
@@ -42,11 +42,11 @@ export const QRCode: FC<QRCodeProps> = () => {
 	return (
 		<span id="reception">
 			<form
-				className="qr-background flex justify-center items-center flex-col p-3 min-h-[100%] pt-32 pb-32"
+				className="qr-background flex justify-center items-center flex-col p-3 min-h-[100%] pt-32 pb-32 max-h-screen"
 				onSubmit={handleFormSubmit}>
 				<h1 className="text-6xl mb-8">Jade & Scotty</h1>
 				{isComplete ? (
-					<h1 className="text-8xl montserrat qr-text">Thank you!</h1>
+					<h1 className="text-5xl montserrat qr-text">Thank you!</h1>
 				) : (
 					<>
 						<h1 className="qr-text text-2xl text-center montserrat pb-3 min-w-md max-w-lg">
@@ -73,6 +73,30 @@ export const QRCode: FC<QRCodeProps> = () => {
 						</button>
 					</>
 				)}
+				<a
+					className="montserrat bg-white text-black font-semibold py-2 px-4 rounded shadow flex w-64 justify-between mx-auto mt-8"
+					href="https://www.amazon.com/wedding/share/jadeandscotty"
+					rel="noopener noreferrer"
+					target="_blank">
+					<img
+						alt="Amazon icon"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/512px-Amazon_icon.svg.png"
+						width="25"
+					/>
+					Our Amazon Registry
+				</a>
+				<a
+					className="montserrat bg-white text-black font-semibold py-2 px-4 rounded shadow flex w-64 justify-between mx-auto mt-8"
+					href="https://www.anthropologie.com/registry/listing?registryId=0CB480FD1905"
+					rel="noopener noreferrer"
+					target="_blank">
+					{/* <img
+					alt="Amazon icon"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/512px-Amazon_icon.svg.png"
+					width="25"
+				/> */}
+					Our Anthropology Registry
+				</a>
 			</form>
 		</span>
 	);
